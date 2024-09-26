b244c76a5cd4fe13cd199c08ec6981cfb18b944d
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-27 03:21:09 +0600

    project basic setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyD0RnabUWEZpuzELd_HYe6kIeQDtxVFQC4",
+	authDomain: "final-exam-09.firebaseapp.com",
+	projectId: "final-exam-09",
+	storageBucket: "final-exam-09.appspot.com",
+	messagingSenderId: "8492524872",
+	appId: "1:8492524872:web:988c73af20b2236191bf7d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root";
+import Home from "./components/Home";
+import { HelmetProvider } from "react-helmet-async";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root></Root>,
+		children: [
+			{
+				path: "/",
+				element: <Home></Home>,
+			},
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<HelmetProvider>
+			<RouterProvider router={router} />
+		</HelmetProvider>
+	</StrictMode>
+);
