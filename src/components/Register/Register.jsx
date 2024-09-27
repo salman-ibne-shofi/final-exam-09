8e753924232b86848f72a96d147ade42db3afff9
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-28 05:53:18 +0600

    register field updated

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,7 +5,11 @@ const Register = () => {
 		e.preventDefault();
 		console.log(e.currentTarget);
 		const form = new FormData(e.currentTarget);
-		console.log(form.get("password"));
+		const name = form.get("name");
+		const email = form.get("email");
+		const photo = form.get("photo");
+		const password = form.get("password");
+		console.log(name, email, photo, password);
 	};
 
 	return (
