acfe00fb6f052e6654b5d9ef61cd6e726ad15df4
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-10-04 01:58:59 +0600

    footer section modification done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,57 +1,94 @@
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { MdHomeWork } from "react-icons/md";
+
 const Footer = () => {
 	return (
 		<footer className="footer bg-base-300 p-10 rounded-2xl">
-			<nav>
-				<h6 className="footer-title">Services</h6>
-				<a className="link link-hover">Branding</a>
-				<a className="link link-hover">Design</a>
-				<a className="link link-hover">Marketing</a>
-				<a className="link link-hover">Advertisement</a>
-			</nav>
-			<nav>
-				<h6 className="footer-title">Company</h6>
-				<a className="link link-hover">About us</a>
-				<a className="link link-hover">Contact</a>
-				<a className="link link-hover">Jobs</a>
-			</nav>
-			<nav>
-				<h6 className="footer-title">Social</h6>
-				<div className="grid grid-flow-col gap-4">
+			<div>
+				<div className="flex">
+					<img
+						className="h-10 mr-2"
+						src="https://i.postimg.cc/nVW9kMv9/9f701e379dace43168d606836a42b36e-removebg-preview.png"
+						alt=""
+					/>
+					<h6 className="footer-title text-2xl">
+						Property & Real Estate
+					</h6>
+				</div>
+
+				<div className="grid grid-flow-col gap-6 mx-auto">
 					<a>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							className="fill-current"
-						>
-							<path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-						</svg>
+						<FaFacebookF className="w-8 h-8" />
 					</a>
 					<a>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							className="fill-current"
-						>
-							<path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-						</svg>
+						<FaTwitter className="w-8 h-8" />
 					</a>
 					<a>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							className="fill-current"
-						>
-							<path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-						</svg>
+						<GrInstagram className="w-8 h-8" />
+					</a>
+					<a>
+						<FaLinkedin className="w-8 h-8" />
+					</a>
+					<a>
+						<FaYoutube className="w-8 h-8" />
 					</a>
 				</div>
-			</nav>
+				<div className="flex mx-auto gap-2">
+					<MdHomeWork className="w-5 h-5" />
+					<p>Equal Housing Opportunity</p>
+				</div>
+				<div className="border-orange-400 border-t px-10 py-2 mx-auto">
+					Copyright © 2024 - All right reserved
+				</div>
+			</div>
+			<div>
+				<h6 className="footer-title border-orange-400 border-b">
+					Advertisers
+				</h6>
+				<a className="link link-hover">Advertise</a>
+				<a className="link link-hover">Add a Property</a>
+				<a className="link link-hover">Digital Feeds Program</a>
+				<a className="link link-hover">Community</a>
+				<a className="link link-hover">Customers Portal</a>
+			</div>
+			<div>
+				<h6 className="footer-title border-orange-400 border-b">
+					The Marketplace
+				</h6>
+				<a className="link link-hover">ForRent</a>
+				<a className="link link-hover">Homes.com</a>
+				<a className="link link-hover">Land.com</a>
+				<a className="link link-hover">Apartment.com</a>
+				<a className="link link-hover">Flat.com</a>
+				<a className="link link-hover">Plot.com</a>
+			</div>
+			<div>
+				<h6 className="footer-title border-orange-400 border-b">
+					Rental Manager
+				</h6>
+				<a className="link link-hover">Rental Manager</a>
+				<a className="link link-hover">List Your Property For Rent</a>
+				<a className="link link-hover">Screen Applicant</a>
+				<a className="link link-hover">Create Rental Leases</a>
+				<a className="link link-hover">Collect Rent Online</a>
+				<a className="link link-hover">Rent Comps</a>
+				<a className="link link-hover">Rental Manager Resources</a>
+				<a className="link link-hover">Rental Property Calculator</a>
+			</div>
+			<div>
+				<h6 className="footer-title border-orange-400 border-b">
+					Company
+				</h6>
+				<a className="link link-hover">About us</a>
+				<a className="link link-hover">Contact</a>
+				<a className="link link-hover">Careers</a>
+				<a className="link link-hover">Legal Notice</a>
+				<a className="link link-hover">Privacy Notice</a>
+			</div>
 		</footer>
 	);
 };
